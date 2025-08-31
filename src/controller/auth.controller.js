1669b49dd95faf3010993d024106bc43ed9e1307
deepmali890 <deepmali590@gmail.com>
2025-08-31 21:52:17 +0530

    fix cookies

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -66,10 +66,11 @@ exports.register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
+
 
     await newUser.save();
 
@@ -241,10 +242,11 @@ exports.login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
+
 
 
 
@@ -542,10 +544,11 @@ exports.googleAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
+
 
 
 
